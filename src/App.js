7660cb930792cdab8faa5845828bbdff76d91648
mani2002 <manikantavinjamuri8522@gmail.com>
2024-03-05 11:20:44 +0530

    added corosel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import HomeNav1 from "./components/HomeNav1";
+import HomeNav2 from "./components/HomeNav2";
+import HomeCarousels from "./components/HomeCarousels";
 
 function App() {
   return (
     <div>
       <HomeNav1 />
+      <HomeNav2 />
+      <HomeCarousels />
     </div>
   );
 }
---
+++ src/components/HomeNav2.js
@@ -0,0 +1,168 @@
+import React from "react";
+
+function HomeNav2() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#232F3E",
+        padding: "1vh",
+        display: "flex",
+        justifyContent: "space-between",
+        width: "100vw",
+      }}
+    >
+      <button
+        className="btn"
+        style={{
+          backgroundColor: "#232F3E",
+          color: "white",
+          fontSize: "2.2vh",
+        }}
+      >
+        All
+      </button>
+      <button
+        className="btn"
+        style={{
+          backgroundColor: "#232F3E",
+          color: "white",
+          fontSize: "2.2vh",
+        }}
+      >
+        Fresh
+      </button>
+      <button
+        className="btn"
+        style={{
+          backgroundColor: "#232F3E",
+          color: "white",
+          fontSize: "2.2vh",
+        }}
+      >
+        Amazon miniTV
+      </button>
+      <button
+        className="btn"
+        style={{
+          backgroundColor: "#232F3E",
+          color: "white",
+          fontSize: "2.2vh",
+        }}
+      >
+        Sell
+      </button>
+      <button
+        className="btn"
+        style={{
+          backgroundColor: "#232F3E",
+          color: "white",
+          fontSize: "2.2vh",
+        }}
+      >
+        Best Sellers
+      </button>
+      <button
+        className="btn"
+        style={{
+          backgroundColor: "#232F3E",
+          color: "white",
+          fontSize: "2.2vh",
+        }}
+      >
+        Mobiles
+      </button>
+      <button
+        className="btn"
+        style={{
+          backgroundColor: "#232F3E",
+          color: "white",
+          fontSize: "2.2vh",
+        }}
+      >
+        Today's Deals
+      </button>
+      <button
+        className="btn"
+        style={{
+          backgroundColor: "#232F3E",
+          color: "white",
+          fontSize: "2.2vh",
+        }}
+      >
+        Electronics
+      </button>
+      <button
+        className="btn"
+        style={{
+          backgroundColor: "#232F3E",
+          color: "white",
+          fontSize: "2.2vh",
+        }}
+      >
+        Customer Service
+      </button>
+      <button
+        className="btn"
+        style={{
+          backgroundColor: "#232F3E",
+          color: "white",
+          fontSize: "2.2vh",
+        }}
+      >
+        Prime
+      </button>
+      <button
+        className="btn"
+        style={{
+          backgroundColor: "#232F3E",
+          color: "white",
+          fontSize: "2.2vh",
+        }}
+      >
+        New Releases
+      </button>
+      <button
+        className="btn"
+        style={{
+          backgroundColor: "#232F3E",
+          color: "white",
+          fontSize: "2.2vh",
+        }}
+      >
+        Gift Ideas
+      </button>
+      <button
+        className="btn"
+        style={{
+          backgroundColor: "#232F3E",
+          color: "white",
+          fontSize: "2.2vh",
+        }}
+      >
+        Home & Kitchen
+      </button>
+      <button
+        className="btn"
+        style={{
+          backgroundColor: "#232F3E",
+          color: "white",
+          fontSize: "2.2vh",
+        }}
+      >
+        Fashion
+      </button>
+      <button
+        className="btn"
+        style={{
+          backgroundColor: "#232F3E",
+          color: "white",
+          fontSize: "2.2vh",
+        }}
+      >
+        Amazon Pay
+      </button>
+    </div>
+  );
+}
+
+export default HomeNav2;
